59054f7a405d24b95a0686e503b5fff5f6ac4641
seungmin-kr <tmdals3320@naver.com>
2022-08-02 15:40:29 +0900

    refactor: DTO 이름 변경 및 미사용 구문 삭제
    가독성을 위해
    RestDTO -> RestaurantDTO
    RestMenuDTO -> MenuDTO
    변경 및 사용하지 않는 어노테이션과 import 구문 삭제

--- exSpring/src/main/java/com/Board/dto/MenuDTO.java
+++ exSpring/src/main/java/com/Board/dto/MenuDTO.java
@@ -1,14 +1,10 @@
 package com.Board.dto;
 
-import org.apache.ibatis.type.Alias;
-
 import lombok.Getter;
 import lombok.Setter;
 
-@Alias("RestMenuDTO")
 @Getter
-@Setter
-public class RestMenuDTO {
+public class MenuDTO {
 //	rest_menu 테이블
 	String restId;
 	String menuId;
